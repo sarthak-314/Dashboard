0c17d25c4ab38833501696e6357f1c488d41bc52
Readoc <sarthak.bhatt314@gmail.com>
2020-12-03 20:14:05 +0530

    Updates

---
+++ precache-manifest.1f9ec17bcfe64eacae9c18f8c9143f04.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c10099c5ab16c49c788d0495f07baea3",
+    "url": "/Dashboard/index.html"
+  },
+  {
+    "revision": "551e11d7c9d8d5ea9247",
+    "url": "/Dashboard/static/css/2.d0176e96.chunk.css"
+  },
+  {
+    "revision": "5dc96d881da64fbc6fe7",
+    "url": "/Dashboard/static/css/main.8e9f8467.chunk.css"
+  },
+  {
+    "revision": "551e11d7c9d8d5ea9247",
+    "url": "/Dashboard/static/js/2.96fbcb61.chunk.js"
+  },
+  {
+    "revision": "34491c6de1fbcf2388878b87785c74e7",
+    "url": "/Dashboard/static/js/2.96fbcb61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5dc96d881da64fbc6fe7",
+    "url": "/Dashboard/static/js/main.d9e8716f.chunk.js"
+  },
+  {
+    "revision": "ab85ba1260b966cf962e",
+    "url": "/Dashboard/static/js/runtime-main.4d8f3eee.js"
+  },
+  {
+    "revision": "7f91d2d23d1e7fb860807f8a63d96d36",
+    "url": "/Dashboard/static/media/marc.7f91d2d2.jpg"
+  },
+  {
+    "revision": "8e9f0a3c5578a20733d5bad0e51c91fb",
+    "url": "/Dashboard/static/media/sidebar-1.8e9f0a3c.jpg"
+  },
+  {
+    "revision": "310509c95512893dc661bd3a6b0d2a5d",
+    "url": "/Dashboard/static/media/sidebar-2.310509c9.jpg"
+  },
+  {
+    "revision": "2503169017014036cf0dd0dd7c2a2b8a",
+    "url": "/Dashboard/static/media/sidebar-3.25031690.jpg"
+  },
+  {
+    "revision": "fc9cb0538eb5a4dfd6fbb1bf6dd6189b",
+    "url": "/Dashboard/static/media/sidebar-4.fc9cb053.jpg"
+  }
+]);
